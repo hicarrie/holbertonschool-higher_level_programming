30ef0d30e512f8d2a7f6d67fa7469f5631480a3c
Carrie Ybay <carrieybay@gmail.com>
2017-05-02 13:05:33 -0700

    Fix string formatting

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,11 +6,10 @@ if number < 0:
 else:
     last = number % 10
 if last == 0:
-    print('Last digit of ' + '{:d}'.format(number) + ' is '
-          + '{:d}'.format(last) + ' and is 0')
+    print('Last digit of {:d} is {:d} and is 0'.format(number, last))
 elif last > 5:
-    print('Last digit of ' + '{:d}'.format(number) + ' is '
-          + '{:d}'.format(last) + ' and is greater than 5')
+    print('Last digit of {:d} is {:d} and is greater than 5'
+          .format(number, last))
 else:
-    print('Last digit of ' + '{:d}'.format(number) + ' is '
-          + '{:d}'.format(last) + ' and is less than 6 and not 0')
+    print('Last digit of {:d} is {:d} and is less than 6 and not 0'
+          .format(number, last))
