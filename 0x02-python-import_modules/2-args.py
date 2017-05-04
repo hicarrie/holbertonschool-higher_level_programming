913431928527db5dfeb6630046566d6a54117ede
Carrie Ybay <carrieybay@gmail.com>
2017-05-04 12:15:16 -0700

    Fix statements not printing correctly

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,11 @@ if __name__ == "__main__":
     import sys
     count = len(sys.argv) - 1
     if count < 2:
-        print("{:d} argument.".format(count))
+        if count == 0:
+            print("{:d} argument.".format(count))
         if count == 1:
-            print("{:s}".format(sys.argv[1]))
+            print("{:d} argument:".format(count))
+            print("{:s}".format(sys.argv[count]))
     else:
         print("{:d} arguments:".format(count))
         for i in range(1, count + 1):
