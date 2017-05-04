ba8ec1ef22f3beb8f3a9d8913c72be0cc3b7371c
Carrie Ybay <carrieybay@gmail.com>
2017-05-04 12:58:45 -0700

    Refactor code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    count = len(sys.argv) - 1
-    if count < 2:
-        if count == 0:
-            print("{:d} argument.".format(count))
-        if count == 1:
-            print("{:d} argument:".format(count))
-            print("{:s}".format(sys.argv[count]))
+    count = len(sys.argv)
+    if count == 1:
+        print("0 argument.")
+    elif count == 2:
+        print("1 argument:")
     else:
-        print("{:d} arguments:".format(count))
-        for i in range(1, count + 1):
-            print("{:d}: {:s}".format(i, sys.argv[i]))
+        print("{:d} arguments:".format(count - 1))
+    for i in range(1, count):
+        print("{:d}: {:s}".format(i, sys.argv[i]))
