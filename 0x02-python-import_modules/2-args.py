379c6f4749ee80fbd1eb5c05334771862cbe0622
Carrie Ybay <carrieybay@gmail.com>
2017-05-04 11:58:26 -0700

    Refactor code

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,8 @@ if __name__ == "__main__":
     import sys
     count = len(sys.argv) - 1
     if count < 2:
-        if count == 0:
-            print("{:d} argument.".format(count))
+        print("{:d} argument.".format(count))
         if count == 1:
-            print("{:d} argument:".format(count))
             print("{:s}".format(sys.argv[1]))
     else:
         print("{:d} arguments:".format(count))
