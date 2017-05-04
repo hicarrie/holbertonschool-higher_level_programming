631731fdfcbfffcebc95df81736357c9512d581f
Carrie Ybay <carrieybay@gmail.com>
2017-05-04 11:19:56 -0700

    Initial commit

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    count = len(sys.argv) - 1
+    if count < 2:
+        if count == 0:
+            print("{:d} argument.".format(count))
+        if count == 1:
+            print("{:d} argument:".format(count))
+            print("{:s}".format(sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(count))
+        for i in range(1, count + 1):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
