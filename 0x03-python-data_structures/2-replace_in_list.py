bf50af68f17ea8f337e2d69fc86c47d6df119ef9
Carrie Ybay <carrieybay@gmail.com>
2017-05-09 12:28:24 -0700

    Edit to account for edge cases

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx <= len(my_list):
+    if idx >= len(my_list) or idx < 0 or len(my_list) == 0:
+        return my_list
+    else:
         my_list[idx] = element
-    return my_list
+        return my_list
