c3cbdb9ced5ce8171c25763d882607722615ef25
Carrie Ybay <carrieybay@gmail.com>
2017-05-09 21:13:52 -0700

    Edit to account for empty rows

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if len(matrix) == 0:
-        print("\n", end="")
+        print("")
     for row in matrix:
-        for i in range(len(row)):
-            if row[i] != "":
+        if len(row) == 0:
+            print("")
+        else:
+            for i in range(len(row)):
                 print("{:d}".format(row[i]), end="")
-        print("\n", end="")
+            print("")
