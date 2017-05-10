49642b61e2a80425393b9ee2d4ae4c89f854a101
Carrie Ybay <carrieybay@gmail.com>
2017-05-09 17:45:37 -0700

    Edit to account for empty string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    return (len(sentence), sentence[0])
+    length = len(sentence)
+    if length == 0:
+        first = "None"
+    else:
+        first = sentence[0]
+    return (length, first)
