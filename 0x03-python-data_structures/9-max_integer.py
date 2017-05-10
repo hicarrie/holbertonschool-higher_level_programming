75004b7d60f6f7b57f5a7ad55dd0e4835ac0940b
Carrie Ybay <carrieybay@gmail.com>
2017-05-09 18:00:59 -0700

    Refactor to use sort method

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,5 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    biggest = my_list[0]
-    for i in my_list:
-        if i > biggest:
-            biggest = i
-    return biggest
+    my_list.sort()
+    return my_list[len(my_list) -1]
