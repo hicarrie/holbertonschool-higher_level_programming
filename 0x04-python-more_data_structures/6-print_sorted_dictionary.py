1634fa4eb7a94b9a3ff66e2301e85846f8a67d82
Carrie Ybay <carrieybay@gmail.com>
2017-05-11 16:25:26 -0700

    Add print statement

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(my_dict):
     if my_dict is None or len(my_dict) == 0:
+        print()
         return
     sorted(my_dict)
     for k, v in my_dict.items():
