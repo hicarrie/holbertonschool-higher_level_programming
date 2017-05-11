e282d4bbb45d36a80d57aa8b67536d4f4fbb6750
Carrie Ybay <carrieybay@gmail.com>
2017-05-11 14:11:01 -0700

    Initial commit

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    change = lambda match: True if x == search else False
+    for x in my_list:
+        if change(x) is True:
+            new_list.append(replace)
+        else:
+            new_list.append(x)
+    return new_list
