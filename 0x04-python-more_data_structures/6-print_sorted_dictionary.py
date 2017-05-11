b941553ca37f3f4575f5e80136a1ad0ba9f780e6
Carrie Ybay <carrieybay@gmail.com>
2017-05-11 16:24:39 -0700

    Edit to account for empty dict

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(my_dict):
+    if my_dict is None or len(my_dict) == 0:
+        return
     sorted(my_dict)
     for k, v in my_dict.items():
         print("{}: {}".format(k, v))
