c2d5e0a0401a213b58d50a30bcfbb21e22ca3666
Carrie Ybay <carrieybay@gmail.com>
2017-05-11 16:25:59 -0700

    Revert back

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(my_dict):
-    if my_dict is None or len(my_dict) == 0:
-        print()
-        return
     sorted(my_dict)
     for k, v in my_dict.items():
         print("{}: {}".format(k, v))
