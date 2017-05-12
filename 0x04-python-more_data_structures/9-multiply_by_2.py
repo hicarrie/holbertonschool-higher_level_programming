d2a4489158c09f2fe7c5aa9632c5fdf94374ef35
Carrie Ybay <carrieybay@gmail.com>
2017-05-11 22:23:13 -0700

    Refactor code

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(my_dict):
-    mul = lambda x: x * 2
-    new_dict = {k: mul(v) for k, v in my_dict.items()}
+    new_dict = {k: v * 2 for k, v in my_dict.items()}
     return new_dict
