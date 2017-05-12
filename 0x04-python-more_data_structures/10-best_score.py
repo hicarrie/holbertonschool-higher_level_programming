c76dd5c4f60bf488f6db5301d592462fbd40bd18
Carrie Ybay <carrieybay@gmail.com>
2017-05-11 22:28:03 -0700

    Edit checks for edge cases

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def best_score(my_dict):
-    if my_dict is None:
+    if my_dict is None or len(my_dict) == 0:
         return None
-    if len(my_dict) == 0:
-        return sorted(my_dict, key=lambda score: score[2])[0]
-    else:
-        return sorted(my_dict, key=lambda score: score[2])[-1]
+    return sorted(my_dict, key=lambda score: score[2])[-1]
