3a9bada7db692e4c67c6a0526da58dc18dbcbc56
Carrie Ybay <carrieybay@gmail.com>
2017-05-12 18:07:33 -0700

    Fix formatting for PEP8 standards

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,9 +3,8 @@ def search_replace(my_list, search, replace):
     if my_list is None:
         return
     new_list = []
-    change = lambda match: True if x == search else False
     for x in my_list:
-        if change(x) is True:
+        if x == search:
             new_list.append(replace)
         else:
             new_list.append(x)
