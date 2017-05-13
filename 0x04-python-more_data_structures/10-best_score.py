adf984903f20b8aeaebc9d7868572db187ff9f3d
Carrie Ybay <carrieybay@gmail.com>
2017-05-12 18:11:55 -0700

    Revert back

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,4 @@
 def best_score(my_dict):
     if my_dict is None or len(my_dict) == 0:
         return None
-    return sorted(my_dict, key=lambda score: score[1])[-1]
+    return sorted(my_dict, key=lambda score: score[2])[-1]
