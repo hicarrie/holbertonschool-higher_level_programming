c8bc3dc2df2a86d1071cdee0dfb57a6d5fcd14df
Carrie Ybay <carrieybay@gmail.com>
2017-05-13 15:17:57 -0700

    Fix iteration to go to next item

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,14 +2,12 @@
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     length = sum(1 for x in my_list)
-    if length == x:
-        x = x - 1
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
         except (ValueError, TypeError):
+            i = i + 1
             count = count - 1
-            continue
         finally:
             count = count + 1
     print()
