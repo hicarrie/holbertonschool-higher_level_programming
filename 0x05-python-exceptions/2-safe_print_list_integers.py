a3dcf42b32ca40080f0ab6409c043ad965c2b1dd
Carrie Ybay <carrieybay@gmail.com>
2017-05-12 20:26:09 -0700

    Fix exception type

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end="")
-        except (ValueError, IndexError):
+        except (ValueError, TypeError):
             count = count - 1
             continue
         finally:
