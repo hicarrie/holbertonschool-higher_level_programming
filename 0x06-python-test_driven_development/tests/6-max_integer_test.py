ec35feb3c217287e1ee125563d2c0f6f4eb329d0
Carrie Ybay <carrieybay@gmail.com>
2017-05-16 19:05:29 -0700

    Remove unnecessary file

--- 0x06-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x06-python-test_driven_development/tests/6-max_integer_test.py
@@ -5,11 +5,10 @@ import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
-    def test_lists(self):
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
-        # with self.assertRaises(TypeError):
-            # max_integer("Hello")
+    self.assertEqual([1, 2, 3, 4], 4)
+    self.assertEqual([1, 3, 4, 2], 4)
+    with self.assertRaises(TypeError):
+        self.assertFalse(all(isinstance(item, int) for item in ["Hello"]))
 
 if __name__ == '__main__':
     unittest.main()
