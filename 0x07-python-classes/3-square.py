88cc9f398a3595208017444481cc5d74322122b8
Carrie Ybay <carrieybay@gmail.com>
2017-05-22 14:03:48 -0700

    Add documentation

--- 0x07-python-classes/3-square.py
+++ 0x07-python-classes/3-square.py
@@ -13,5 +13,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
+    """ returns area of the square """
     def area(self):
         return self.__size ** 2
