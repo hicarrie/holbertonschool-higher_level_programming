17716043b16306aacc7b2b632aa3f09d60163b51
Carrie Ybay <carrieybay@gmail.com>
2017-05-22 13:54:25 -0700

    Initial commit

---
+++ 0x07-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Defines class Square
+"""
+
+
+class Square:
+    """ defines optional private size attribute """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
