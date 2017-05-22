21f24e35a7f2a66dfa0c3cd7c79324c39f74d047
Carrie Ybay <carrieybay@gmail.com>
2017-05-22 13:43:28 -0700

    Initial commit

---
+++ 0x07-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Defines an empty class Square with size attribute
+"""
+
+
+class Square:
+    """ Defines Square class with private size attribute """
+    def __init__(self, size):
+        self.__size = size
