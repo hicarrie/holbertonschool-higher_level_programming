512031052b910b87c3baed8a472e278c7a003161
Carrie Ybay <carrieybay@gmail.com>
2017-05-24 14:36:54 -0700

    Remove extra character when width or height is 0

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -51,7 +51,7 @@ class Rectangle:
     def __str__(self):
         rectangle = []
         if self.__width == 0 or self.__height == 0:
-            return "\n"
+            return ""
         else:
             for i in range(self.__height):
                 for j in range(self.__width):
