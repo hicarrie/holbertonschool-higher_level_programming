896a3444d49ff872a8208c21dc2b4015feeab831
Carrie Ybay <carrieybay@gmail.com>
2017-05-30 14:20:18 -0700

    Initial commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Defines class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """ initializes class """
+    def __init__(self):
+        pass
+
+    """ public instance method area """
+    def area(self):
+        raise Exception("area() is not implemented")
