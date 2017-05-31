d84d05de40aa6130bc16c5db15ac90b444d94ade
Carrie Ybay <carrieybay@gmail.com>
2017-05-31 16:29:36 -0700

    Add function documentation

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module that saves on object to a text file using JSON representation
+Module that saves an object to a text file using JSON representation
 """
 
 
@@ -8,5 +8,6 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """ saves an object to a text file using JSON representation """
     with open(filename, encoding="UTF-8", mode="w") as a_file:
         a_file.write(json.dumps(my_obj))
