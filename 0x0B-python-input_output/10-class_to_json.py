1cfc1b8a40e79999799eee9d0a981d16f8a6dc7b
Carrie Ybay <carrieybay@gmail.com>
2017-06-02 15:06:22 -0700

    Fix output to be dict instead of str

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -11,4 +11,5 @@ import json
 def class_to_json(obj):
     """ function that returns dictionary description with simple
     data structure for JSON serialization of an object """
-    return json.dumps(obj.__dict__)
+    json_obj = json.dumps(obj.__dict__)
+    return json.loads(json_obj)
