1c5eff2e36d37250f1063cb6d590d4239ddd2961
Carrie Ybay <carrieybay@gmail.com>
2017-06-02 11:16:03 -0700

    Remove documentation

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-"""
-Module that checks if an object is an instance of a class inherited
-from the specified class
-"""
-
-
 def inherits_from(obj, a_class):
     """ returns True if obj is an instance of a_class or a class inherited
     from a_class"""
