cf0ca78d581d2f725015f62d18fcb8c631c35640
Carrie Ybay <carrieybay@gmail.com>
2017-06-02 16:52:12 -0700

    Initial commit

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Defines class Student
+"""
+
+
+import json
+
+
+class Student:
+    """ initializes instance """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ function that returns dictionary description with simple
+        data structure for JSON serialization of an object """
+        if attrs is None:
+            json_obj = json.dumps(self.__dict__)
+            return json.loads(json_obj)
+        else:
+            # should return given attributes in dict format :(
+            json_obj = json.dumps(self.__dict__)
+            return json.loads(json_obj)
