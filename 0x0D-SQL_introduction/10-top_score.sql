eeac7af020394dd52d6488c7f2bdd2bd485336c1
Carrie Ybay <carrieybay@gmail.com>
2017-07-12 15:48:11 -0700

    Initial commit

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- lists all records of the table 'second_table' in database 'hbtn_0c_0',
+-- displaying both name and score and ordering by score
+SELECT score, name FROM second_table ORDER BY score DESC;
