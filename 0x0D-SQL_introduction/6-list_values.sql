3a5cafda957801b0e91d0eef73ed8f533d2e731d
Carrie Ybay <carrieybay@gmail.com>
2017-07-12 14:30:37 -0700

    Initial commit

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- lists all rows of the table `first table` from the database `hbtn_0c_0`
+SELECT * FROM `first table`;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- inserts a new row in table 'first table' in database 'hbtn_0c_0'
+INSERT INTO `first table` (`id`, `name`) VALUES (89, 'Holberton School');
