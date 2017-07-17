53a227eca3a9cc881cfcc8d2e24d433a81cfab89
Carrie Ybay <carrieybay@gmail.com>
2017-07-17 11:07:25 -0700

    Add join

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,5 +2,6 @@
 -- linked to each
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_shows
 FROM tv_genres, tv_show_genres
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name, tv_show_genres.genre_id
 ORDER BY number_shows DESC;
