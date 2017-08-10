1ec1b6a46e5a6dbc556627a9ee5e55f111714306
Carrie Ybay <carrieybay@gmail.com>
2017-08-10 16:45:00 -0700

    Add missing semicolon

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
                          passwd=passwd, db=database)
     cur = db.cursor()
 
-    sql_cmd = "SELECT * FROM states WHERE name='{}' ORDER BY id".format(name)
+    sql_cmd = "SELECT * FROM states WHERE name='{}' ORDER BY id;".format(name)
     cur.execute(sql_cmd)
     states = cur.fetchall()
     for state in states:
