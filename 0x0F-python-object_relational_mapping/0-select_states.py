9c1bc5532a5fe7817d9692da2a47e0a85b9f212a
Carrie Ybay <carrieybay@gmail.com>
2017-08-10 14:48:26 -0700

    Initial commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module for script that lists all states from database hbtn_0e_0_usa
+"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    host = "localhost"
+    port = 3306
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(host=host, port=port, user=user,
+                         passwd=passwd, db=database)
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY states.id;")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
