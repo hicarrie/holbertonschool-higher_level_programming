cece98bc38d3ae5b8aa85699f7281ea0952b7bf5
Carrie Ybay <carrieybay@gmail.com>
2017-08-10 16:57:13 -0700

    Initial commit

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module for script that takes an argument and displays all value in the
+states table where name matches the argument
+"""
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    host = "localhost"
+    port = 3306
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(host=host, port=port, user=user,
+                         passwd=passwd, db=database)
+    cur = db.cursor()
+
+    sql_cmd = "SELECT cities.id, cities.name, states.name FROM cities, states\
+               WHERE cities.state_id=states.id ORDER BY cities.id"
+    cur.execute(sql_cmd)
+    states = cur.fetchall()
+    for city in cities:
+        print(city)
+    cur.close()
+    db.close()
