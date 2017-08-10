524407e50ca8fb8381dbe9d2a920c50a320ce4f2
Carrie Ybay <carrieybay@gmail.com>
2017-08-10 16:26:25 -0700

    Fix SQL query to be case sensitive

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,8 @@ if __name__ == "__main__":
                          passwd=passwd, db=database)
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id;")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'\
+                ORDER BY id;")
     states = cur.fetchall()
     for state in states:
         print(state)
