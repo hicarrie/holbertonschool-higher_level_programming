d5d3848f2df71118586614ad5f7320c9d0dba48b
Carrie Ybay <carrieybay@gmail.com>
2017-08-11 16:09:37 -0700

    Remove Column names

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -13,11 +13,9 @@ class City(Base):
     """ defines City class """
 
     __tablename__ = "cities"
-    id = Column("id", Integer, autoincrement=True, nullable=False,
-                primary_key=True)
-    name = Column("name", String(128), nullable=False)
-    state_id = Column("state_id", Integer, ForeignKey("states.id"),
-                      nullable=False)
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
 
     state = relationship("State")
 
