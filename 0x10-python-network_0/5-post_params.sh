58e65a721c95f3cfbc260f0045d92e6537d75add
Carrie Ybay <carrieybay@gmail.com>
2017-08-22 17:04:09 -0700

    Edit to pass shellcheck

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request and displays body of the response
-curl -s $1 -X POST -d "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD"
+curl -s "$1" -X POST -d "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD"
