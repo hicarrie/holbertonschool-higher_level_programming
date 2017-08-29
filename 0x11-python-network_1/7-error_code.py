e66b2f5ecdbd93619bd07240f5b29ba53c1754c8
Carrie Ybay <carrieybay@gmail.com>
2017-08-28 20:36:57 -0700

    Initial commit

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Sends a request to a URL and displays the body of the response
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
