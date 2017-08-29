18450503486326749ec47d72e847ffb818be603b
Carrie Ybay <carrieybay@gmail.com>
2017-08-28 18:04:25 -0700

    Add check for __name__

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,5 +6,6 @@ import sys
 import urllib.request
 
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.headers['X-Request-Id'])
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.headers['X-Request-Id'])
