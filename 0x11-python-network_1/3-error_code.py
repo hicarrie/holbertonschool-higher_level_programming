c234760505bf9c56f13c5cbb2c25d61f9bf9bfbb
Carrie Ybay <carrieybay@gmail.com>
2017-08-28 19:47:46 -0700

    Initial commit

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Sends a request to a URL and displays the body of the response
+
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read())
+    except urllib.error.HTTPError as e:
+        print('Error code: {}'.format(e.code))
