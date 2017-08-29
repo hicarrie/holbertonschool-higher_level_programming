90902b51d28dd46ddfae83acbac7edeba4d32282
Carrie Ybay <carrieybay@gmail.com>
2017-08-28 19:32:53 -0700

    Initial commit

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Sends a POST request to a URL and displays the body of the response
+
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    data = sys.argv[2]
+    data = data.encode('utf8')
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read())
