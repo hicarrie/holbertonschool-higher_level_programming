7759a658f534734e8db7e59fc0853a82ba851141
Carrie Ybay <carrieybay@gmail.com>
2017-08-30 16:03:54 -0700

    Fix parsing of email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,8 +9,9 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    data = sys.argv[2]
-    data = data.encode('utf8')
+    values = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
     req = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf8'))
