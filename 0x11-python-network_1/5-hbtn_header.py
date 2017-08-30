90668b71532534894cca33df49094620eae03480
Carrie Ybay <carrieybay@gmail.com>
2017-08-30 16:07:04 -0700

    Add try/except

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -10,4 +10,7 @@ import requests
 
 if __name__ == "__main__":
     r = requests.get(sys.argv[1])
-    print(r.headers['X-Request-Id'])
+    try:
+        print(r.headers['X-Request-Id'])
+    except:
+        return
