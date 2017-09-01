a833c700798f4497ece73ec7404c7013021c5c4d
Carrie Ybay <carrieybay@gmail.com>
2017-09-01 15:41:58 -0700

    Initial commit

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Sends a search request to the Star Wars API
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/user/'
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    r = requests.get(url, auth=(user, pwd))
+    r_json = r.json()
+    print(r_json.get('id'))
