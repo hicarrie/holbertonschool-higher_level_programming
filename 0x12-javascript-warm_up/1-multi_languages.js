aaf3cfae5954a9631dfa9f4103eae38a96410c2a
Carrie Ybay <carrieybay@gmail.com>
2017-09-19 11:04:57 -0700

    Initial commit

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const c = 'C is fun';
+const python = 'Python is cool';
+const javascript = 'Javascript is amazing';
+console.log(c);
+console.log(python);
+console.log(javascript);
