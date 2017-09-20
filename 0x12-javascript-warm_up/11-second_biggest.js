869b5bd1c04db28f44cfa3123df156ddd74dfad5
Carrie Ybay <carrieybay@gmail.com>
2017-09-20 09:15:15 -0700

    Fix semistandard formatting

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,24 +1,23 @@
 #!/usr/bin/node
 // searches for the second biggest integer in the list of arguments
-const list = process.argv
+const list = process.argv;
 list.splice(0, 2);
 const length = list.length;
 let largest = 0;
-let next_largest = 0;
+let nextLargest = 0;
 
 if (length === 2 || length === 3) {
   console.log('0');
 } else {
   for (let i = 0; i < length; i++) {
-    if (list[i] > next_largest) {
+    if (list[i] > nextLargest) {
       if (list[i] > largest) {
-	next_largest = largest;
-	largest = list[i];
-      }
-      else {
-	next_largest = list[i];
+        nextLargest = largest;
+        largest = list[i];
+      } else {
+        nextLargest = list[i];
       }
     }
   }
 }
-console.log(next_largest);
+console.log(nextLargest);
