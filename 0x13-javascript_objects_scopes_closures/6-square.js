3b213636bde5534ec7e99328ae0a5a51699a0c24
Carrie Ybay <carrieybay@gmail.com>
2017-09-25 15:04:20 -0700

    Initial commit

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// class that defines a square and inherits from Rectangle
+const Square = require('./5-square').Square;
+
+Square.prototype = Object.create(Square.prototype);
+Square.prototype.constructor = Square;
+Square.prototype.charPrint = function (c = 'X') {
+  for (let i = 0; i < this.height; i++) {
+    for (let j = 0; j < this.width; j++) {
+      process.stdout.write(c);
+    }
+    console.log();
+  }
+};
+
+module.exports.Square = Square;
