b3d0adc3d75aa21d259e4f0e396c44581e935709
Carrie Ybay <carrieybay@gmail.com>
2017-09-26 14:32:24 -0700

    Fix for semistandard formatting

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -11,7 +11,7 @@ request(url, function (error, response, body) {
   } else {
     fs.writeFile(path, body, 'utf8', function (err) {
       if (err) {
-	console.log(err);
+        console.log(err);
       }
     });
   }
