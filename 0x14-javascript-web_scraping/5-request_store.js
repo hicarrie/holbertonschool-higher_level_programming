ff93b34848141e3aa61ee213d7e5bc162e3f92d8
Carrie Ybay <carrieybay@gmail.com>
2017-09-26 14:31:13 -0700

    Initial commit

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// gets the contents of a webpage and stores it in a file
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const path = process.argv[3];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(path, body, 'utf8', function (err) {
+      if (err) {
+	console.log(err);
+      }
+    });
+  }
+});
