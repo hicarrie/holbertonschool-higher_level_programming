59b0f8458970e57cc18e7d16f69fdf0f1c2a649c
Carrie Ybay <carrieybay@gmail.com>
2017-09-26 13:05:31 -0700

    Change let to const

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // reads and prints the content of a file
-let fs = require('fs');
+const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
