d9063ba9a469c124fa37df19761d92ba5cca540d
Carrie Ybay <carrieybay@gmail.com>
2017-09-26 12:58:53 -0700

    Initial commit

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// writes a string to a file
+let fs = require('fs');
+
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
+  if (err) {
+    console.log(err);
+  };
+});
