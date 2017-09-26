42b5541590f5f6b48a98b8d807264161c59d9dae
Carrie Ybay <carrieybay@gmail.com>
2017-09-26 13:04:55 -0700

    Initial commit

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// displays the status code of a GET request
+const request = require('request');
+
+request (process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code: ' + response['statusCode']);
+  }
+});
