ea90bdb3425f4a269b325a87180063f77d38a40e
Carrie Ybay <carrieybay@gmail.com>
2017-09-26 13:16:08 -0700

    Initial commit

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// prints the title of a Star Wars movie where the episode number
+// matches a given integer
+const request = require('request');
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    data = JSON.parse(body);
+    console.log(data['title']);
+  }
+});
