35b8380e94e4856c9c31b48cbb3fa7dbe2552841
Carrie Ybay <carrieybay@gmail.com>
2017-09-27 10:40:22 -0700

    Initial commit

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// returns the reversed version of a list without using reverse built-in
+exports.esrever = function (list) {
+  let length = list.length - 1;
+  for (let i = 0; i < length; i++, length--) {
+    let swap = list[i];
+    list[i] = list[length];
+    list[length] = swap;
+  }
+  return list;
+}
