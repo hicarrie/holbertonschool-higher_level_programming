f7729109d45e93c58308c5639d36a598b60101f5
Carrie Ybay <carrieybay@gmail.com>
2017-09-27 14:13:39 -0700

    Initial commit

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// imports an array and computes a new array
+const list = require('./100-data').list;
+
+const factors = list.map(function (x, i) {
+  return x * i;
+});
+
+console.log(factors);
