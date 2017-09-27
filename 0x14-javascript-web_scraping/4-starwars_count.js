0286cecf6e796492811ad459637bf91496c763a5
Carrie Ybay <carrieybay@gmail.com>
2017-09-27 13:30:45 -0700

    Edit iteration to properly go through all objects

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,18 +3,19 @@
 // matches a given integer
 const request = require('request');
 const url = process.argv[2];
-const wedge = 'https://swapi.co/api/people/18/';
+const wedge = '18';
 
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
     const data = JSON.parse(body).results;
-    const length = data.length;
     let count = 0;
-    for (let i = 0; i < length; i++) {
-      if (data[i]['characters'].includes(wedge)) {
-        count++;
+    for (let film of data) {
+      for (let character of film['characters']) {
+        if (character.includes(wedge)) {
+          count++;
+        }
       }
     }
     console.log(count);
