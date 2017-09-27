b886148977299c93aa47503c2fb0da198243e1c2
Carrie Ybay <carrieybay@gmail.com>
2017-09-27 10:45:50 -0700

    Initial commit

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// converts a number from base 10 to another base passed as an argument
+exports.converter = function (base) {
+  return function (num) {
+    return num.toString(base);
+  }
+}
