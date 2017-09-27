3d6827c9843001bcf94675a7554f0dbcb81c1e26
Carrie Ybay <carrieybay@gmail.com>
2017-09-27 10:48:17 -0700

    Fix semistandard formatting

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,20 +2,20 @@
 // prints the title of a Star Wars movie where the episode number
 // matches a given integer
 const request = require('request');
-const url = 'http://swapi.co/api/films/';
+const url = process.argv[2];
 const wedge = 'https://swapi.co/api/people/18/';
 
 request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    let count = 0;
     const data = JSON.parse(body).results;
     const length = data.length;
+    let count = 0;
     for (let i = 0; i < length; i++) {
       if (data[i]['characters'].includes(wedge)) {
-	count++;
-      };
+        count++;
+      }
     }
     console.log(count);
   }
